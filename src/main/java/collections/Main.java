e1134e37f41dabb52e0b43d863954c6f4268d828
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-07 15:25:55 -0600

    singlyLinkedList and one method for ArrayList

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -3,7 +3,13 @@ package collections;
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
-        sll.createSLL(4);
-        System.out.println(sll.tail.value);
+
+        sll.add(2, 4);
+        sll.add(3, 1);
+        sll.add(6, 0);
+        sll.add(11);
+        sll.add(15);
+        sll.remove(5);
+        sll.print();
     }
 }
