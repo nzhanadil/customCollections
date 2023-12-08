a1195b5e7ca545096bd13c61f2995abd5a88415f
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-08 09:25:11 -0600

    completed CSLL and upgraded SLL

--- src/main/java/collections/CircularSinglyLinkedList.java
+++ src/main/java/collections/CircularSinglyLinkedList.java
@@ -2,14 +2,14 @@ package collections;
 
 import nodes.Node;
 
-public class SinglyLinkedList {
+public class CircularSinglyLinkedList {
 
     private Node head;
     private Node tail;
     private int size;
 
-    public SinglyLinkedList() {
-        size = 0;
+    public CircularSinglyLinkedList() {
+        this.size = 0;
     }
 
     public void add(int e, int index) {
@@ -17,20 +17,20 @@ public class SinglyLinkedList {
         newNode.value = e;
 
         if (head == null) {
+            newNode.next = newNode;
             head = newNode;
             tail = newNode;
-            size = 1;
-        }
-
-        if (index == 0) {
+        } else if (index == 0) {
             newNode.next = head;
+            tail.next = newNode;
             head = newNode;
         } else if (index >= size) {
+            newNode.next = head;
             tail.next = newNode;
             tail = newNode;
         } else {
             Node tempNode = head;
-            for (int i = 0; i < index - 1; i++) {
+            for (int i = 0; i < size - 1; i++) {
                 tempNode = tempNode.next;
             }
             newNode.next = tempNode.next;
@@ -44,46 +44,48 @@ public class SinglyLinkedList {
         newNode.value = e;
 
         if (head == null) {
+            newNode.next = newNode;
             head = newNode;
-            tail = newNode;
-            size = 1;
         } else {
+            newNode.next = head;
             tail.next = newNode;
-            tail = newNode;
-            size++;
         }
+        tail = newNode;
+        size++;
     }
 
     public int get(int index) {
         if (index > size - 1 || index < 0) {
             throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
         }
-
         Node tempNode = head;
+
         for (int i = 0; i < index; i++) {
             tempNode = tempNode.next;
         }
+
         return tempNode.value;
     }
 
     public int remove(int index) {
-        if (index >= size || index < 0) {
+        if (index > size - 1 || index < 0) {
             throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
         }
-        int value = -1;
-
+        int value = 0;
         if (size == 1) {
-            tail = head = null;
+            value = head.value;
+            head.next = head = tail = null;
         } else if (index == 0) {
             value = head.value;
             head = head.next;
+            tail.next = head;
         } else if (index == size - 1) {
             Node tempNode = head;
             for (int i = 0; i < index - 1; i++) {
                 tempNode = tempNode.next;
             }
             value = tempNode.next.value;
-            tempNode.next = null;
+            tempNode.next = head;
             tail = tempNode;
         } else {
             Node tempNode = head;
@@ -103,7 +105,6 @@ public class SinglyLinkedList {
             if (tempNode.value == object) {
                 return true;
             }
-            tempNode = tempNode.next;
         }
         return false;
     }
@@ -114,18 +115,19 @@ public class SinglyLinkedList {
             Node tempNode = head;
             for (int i = 0; i < size; i++) {
                 System.out.print(tempNode.value);
+                tempNode = tempNode.next;
                 if (i != size - 1) {
                     System.out.print(", ");
                 }
-                tempNode = tempNode.next;
             }
-            System.out.println("]");
+            System.out.print("]");
+            System.out.println();
         }
     }
 
     public void delete() {
         if (head != null) {
-            head = tail = null;
+            head = tail = tail.next = null;
             size = 0;
         }
     }
