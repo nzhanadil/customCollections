3f215f1aa4dd6e408f756013eb5f4593f1314ff2
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-12 09:38:07 -0600

    Queue DS using LL

---
+++ src/main/java/collections/queue/QueueWithLL.java
@@ -0,0 +1,32 @@
+package collections.queue;
+
+import collections.linkedList.SinglyLinkedList;
+
+public class QueueWithLL {
+
+    SinglyLinkedList sll;
+
+    public QueueWithLL(){
+        this.sll = new SinglyLinkedList();
+    }
+
+    public boolean isEmpty(){
+        return sll.head == null;
+    }
+
+    public void enqueue(int e){
+        sll.add(e, 0);
+    }
+
+    public int dequeue(){
+        return sll.remove(0);
+    }
+
+    public int peek(){
+        return sll.get(0);
+    }
+
+    public void delete(){
+        sll.delete();
+    }
+}
